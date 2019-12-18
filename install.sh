36ad9cd5b28060f1fcdf6171ee87f0227b18ff96
dandy <daniel.tschertkow@posteo.de>
2019-12-18 21:04:14 +0100

    modified:   install.sh
    basic install finished. Not tested yet

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@
 
 _DISK="/dev/sda"
 _SOURCE="archlinux.org"  # TODO replace with sensible default
+_ZONEINFO="/Europe/Berlin"
+_KEYMAP="de-latin1"
+_LANG="en_US.UTF-8"
+_LOCALE="en_US.UTF-8 UTF-8"
+_HOSTNAME="forge"
 
 usage () {
     echo -e "\nArch install script.\nUsage: install.sh [options]"
@@ -58,7 +63,7 @@ if ping $_SOURCE -c 4 -W 1 > /dev/null 2>&1; then
 else
     echo "failed. Source unreachable."
     # TODO: code for establishing a connection
-    exit 1
+    # exit 1
 fi
 
 timedatectl set-ntp true
@@ -72,15 +77,18 @@ setup_small_disk() {
     local _MNT
     _MNT=/mnt
     # 2097152 = 2 MiB, room for the bootloader ERROR HERE
-    parted --script ${_DISK} mkpart primary ext4 2MiB 99% \
-	&& mkfs.ext4 -q ${_DISK}1 \
-	&& mount ${_DISK}1 $_MNT \
-	&& fallocate -l 512M $_MNT/swapfile \
-	&& mkswap $_MNT/swapfile \
-	&& chmod 0600 $_MNT/swapfile \
-	&& swapon $_MNT/swapfile \
-	&& umount $_MNT
-    return $?
+    if parted --script ${_DISK} mkpart primary ext4 2MiB 99% \
+	    && mkfs.ext4 -F -L system -q ${_DISK}1; then
+
+	mount ${_DISK}1 $_MNT \
+	    && fallocate -l 512M $_MNT/swapfile \
+	    && chmod 0600 $_MNT/swapfile \
+	    && mkswap -f $_MNT/swapfile \
+
+	umount -l $_MNT
+	return $?
+    fi
+    return 1
 }
 
 # wie mache ich Partitionen? / part braucht min(50%, 40gb)
@@ -91,5 +99,60 @@ if [ $_DSIZE -le 137439000000 ]  # smaller than 128 GiB
 then  # everything on a single partition with swapfile
     echo "Setting a partition format for a small disk (< 128 GiB)"
     setup_small_disk
-    echo "setup_small_disk returned: $?"
+    if ! $?; then
+	echo "setup_small_disk failed"
+    fi
 fi
+
+write_config_files() {
+    local _MNT
+    _MNT=${1:-/mnt}
+
+    if [ -e $_MNT/swapfile ] && grep -c "^/swapfile" $_MNT/etc/fstab; then
+	echo "/swapfile none swap defaults 0 0" >> $_MNT/etc/fstab
+    fi
+
+    ln -sf $_MNT/usr/share/zoneinfo${_ZONEINFO} $_MNT/etc/localtime
+    echo "LANG=$_LANG" > $_MNT/etc/locale.conf
+    echo "$_LOCALE" > $_MNT/etc/locale.gen
+    echo "KEYMAP=$_KEYMAP" >> $_MNT/etc/vconsole.conf
+    echo "$_HOSTNAME" > $_MNT/etc/hostname
+    echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$_HOSTNAME.localdomain $_HOSTNAME" > /etc/hosts
+
+    return $?
+}
+
+set_config() {
+    local _MNT
+    _MNT=${1:-/mnt}
+
+    ### chroot enter ###
+    arch-chroot $_MNT
+
+    hwclock --systohc
+    locale-gen
+    usermod --password $(openssl passwd -6 password) root
+    grub-install --target=i386-pc $_DISK
+    grub-mkconfig -o /boot/grub/grub.cfg
+
+    exit
+    ### chroot exit ###
+
+    return $?
+}
+
+bootstrap_system() {
+    local _MNT
+    _MNT=/mnt
+
+    mount $_DISK1 $_MNT \
+	&& pacstrap $_MNT base linux linux-firmware \
+	&& genfstab -U $_MNT > $_MNT/etc/fstab
+
+    write_config_files $_MNT \
+	&& set_config $_MNT
+
+    umount -lR $_MNT
+
+    return $?
+}
